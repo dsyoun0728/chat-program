49412e477e7eec53b04c81dbc9b78e0c4ff4b483
dongsub_youn <es001311@korea.ac.kr>
2021-10-20 11:06:16 +0900

    feat(client): ThreadPool을 이용한 Client class 추가

--- client/src/main/java/Client.java
+++ client/src/main/java/Client.java
@@ -1,5 +1,80 @@
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.channels.SocketChannel;
+import java.nio.charset.Charset;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 public class Client {
+    ExecutorService executorService;
+    SocketChannel socketChannel;
+
+    void startClient() {
+        try {
+            executorService = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
+            socketChannel = SocketChannel.open();
+            socketChannel.configureBlocking(true);
+            socketChannel.connect(new InetSocketAddress("192.168.14.59", 5001));
+        } catch (IOException e) {
+            System.out.println("startClient try-catch block IOException\n\n\n" + e + "\n\n\n");
+            if (socketChannel.isOpen()) { stopClient(); }
+            return;
+        } catch (Exception e) {
+            System.out.println("startClient try-catch block Exception\n\n\n" + e + "\n\n\n");
+            if (socketChannel.isOpen()) { stopClient(); }
+            return;
+        }
+
+        Runnable readRunnable = () -> receive();
+        executorService.submit(readRunnable);
+
+        /*
+        * client가 끊기지 않도록 계속 구동하는 부분
+        * while(true) {  }
+        * */
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    void stopClient() {
+        try {
+            if (socketChannel != null && socketChannel.isOpen()) { socketChannel.close(); }
+        } catch (IOException e) {
+            System.out.println("stopClient IOException\n\n\n" + e + "\n\n\n");
+        } catch (Exception e) {
+            System.out.println("stopClient Exception\n\n\n" + e + "\n\n\n");
+        }
+    }
+
+    void receive() {
+        System.out.println("receive");
+    }
+
+    void send(ByteBuffer byteBufferData) {
+        Runnable writeRunnable = () -> {
+            try {
+//                    Charset charset = Charset.forName("UTF-8");
+//                    ByteBuffer byteBuffer = charset.encode(data);
+//                    socketChannel.write(byteBuffer);
+                socketChannel.write(byteBufferData);
+            } catch(IOException e) {
+                System.out.println("client send IOException\n\n\n" + e + "\n\n\n");
+                stopClient();
+            } catch(Exception e) {
+                System.out.println("client send Exception\n\n\n" + e + "\n\n\n");
+                stopClient();
+            }
+        };
+        executorService.submit(writeRunnable);
+    }
+
     public static void main(String[] args) {
-        System.out.println("Client Class");
+        Client client = new Client();
+        client.startClient();
+//        client.send(Charset.forName("UTF-8").encode("hello"));
     }
 }
