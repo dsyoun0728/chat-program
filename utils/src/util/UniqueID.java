4f0d627224e5152a523ccd085d8afa3b29a1fd88
dongsub_youn <es001311@korea.ac.kr>
2021-10-28 16:26:49 +0900

    feat(util): client에서 보내는 packet에 id 부여할 때 유일성 보장

--- utils/src/util/UniqueID.java
+++ utils/src/util/UniqueID.java
@@ -1,11 +1,23 @@
 package util;
 
+import java.util.HashSet;
 import java.util.Random;
 
 public class UniqueID {
+    private static HashSet<String> uniqueIDSet = new HashSet<>();
+
     public static byte[] randomUniqueID() {
+        int originalSetSize = uniqueIDSet.size();
         byte[] randomByteArray = new byte[3];
         new Random().nextBytes(randomByteArray);
+        if (uniqueIDSet.size() == (2^24)) uniqueIDSet.removeAll(uniqueIDSet);
+        uniqueIDSet.add(getFullUniqueID(randomByteArray));
+
+        while (originalSetSize == uniqueIDSet.size()) {
+            new Random().nextBytes(randomByteArray);
+            uniqueIDSet.add(getFullUniqueID(randomByteArray));
+        }
+
         return randomByteArray;
     }
 
