8758af8b77d52074efcc5f7499f145d6c7541b25
dongsub_youn <es001311@korea.ac.kr>
2021-11-02 14:00:47 +0900

    feat(parser): ResponseParser 구현

--- utils/src/parser/ResponseParser.java
+++ utils/src/parser/ResponseParser.java
@@ -1,38 +1,52 @@
 package parser;
 
-import parser.Parser;
 import util.Function;
 
-public class ResponseParser extends Parser {
-    String status;
-    String functionName;
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.util.ArrayList;
 
-    public ResponseParser(byte[] responsePacketArray) {
-        super(
-                Parser.sliceByteArray(responsePacketArray, 0, 3),
-                responsePacketArray[4],
-                Parser.sliceByteArray(responsePacketArray, 5, 80),
-                Parser.sliceByteArray(responsePacketArray, 86, 34)
-                );
-        this.status = getStatus(responsePacketArray[3]);
-        this.functionName = Function.getFuncionString(responsePacketArray[85]);
-
-        /*// 데이터 참고용 (추후 삭제 예정 부분)
-        System.out.println("uniqueID: " + this.uniqueID);
-        System.out.println("status: " + this.status);
-        System.out.println("lastFlag: " + this.lastFlag);
-        System.out.println("contentsLength: " + this.contentsLength);
-        System.out.println("contents: " + this.contents);
-        System.out.println("optionalInfo: " + this.optionalInfo);
-        System.out.println("functionName: " + this.functionName);*/
+public class ResponseParser implements Parser {
+    @Override
+    public boolean isLast(byte[] byteArray) {
+        return byteArray[1] >> 7 == -1;
     }
 
-    public String getStatus(byte code) {
-        switch (code) {
-            case 20:
-                return "success";
-            default:
-                return "no code";
+    @Override
+    public String getFunctionName(ArrayList<byte[]> byteArrayList) {
+        return Function.getFuncionString(byteArrayList.get(0)[0]);
+    }
+
+    @Override
+    public int getTotalPacketNumber(ArrayList<byte[]> byteArrayList) {
+        return ByteBuffer.wrap(Parser.sliceByteArray(byteArrayList.get(0), 82,  4)).getInt();
+    }
+
+    @Override
+    public byte[] getContents(ArrayList<byte[]> byteArrayList) throws IOException {
+        ByteBuffer byteBuffer = ByteBuffer.allocateDirect(byteArrayList.size() * 80);
+        int position = 0;
+        for (byte[] byteArray : byteArrayList) {
+            byteBuffer.put(byteArray);
+            if (byteBuffer.position() - position != ( byteArray[1] & 127) ) {
+                throw new IOException("\n\npacket의 contents length 맞지 않음\n\n");
+            }
         }
+        byteBuffer.flip();
+        byte[] returnByteArray = new byte[byteBuffer.remaining()];
+        byteBuffer.get(returnByteArray);
+        return returnByteArray;
+    }
+
+    @Override
+    public byte[] getOptionalInfo(ArrayList<byte[]> byteArrayList) {
+        ByteBuffer byteBuffer = ByteBuffer.allocateDirect(byteArrayList.size() * 38);
+        for (byte[] byteArray : byteArrayList) {
+            byteBuffer.put(byteArray);
+        }
+        byteBuffer.flip();
+        byte[] returnByteArray = new byte[byteBuffer.remaining()];
+        byteBuffer.get(returnByteArray);
+        return returnByteArray;
     }
 }
