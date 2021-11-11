f90e4912b236831eb66b204f77af171cd32210c9
dongsub_youn <es001311@korea.ac.kr>
2021-11-11 16:23:26 +0900

    refactor(server): server 단에서 client 클래스 분리

---
+++ server/src/main/java/server/Client.java
@@ -0,0 +1,73 @@
+package server;
+
+import parser.*;
+
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.channels.SelectionKey;
+import java.nio.channels.Selector;
+import java.nio.channels.SocketChannel;
+import java.util.ArrayList;
+
+public class Client {
+    private SocketChannel socketChannel;
+    private SelectionKey selectionKey;
+    private Parser requestParser = new RequestParser();
+    private String userNick;
+    private ArrayList<byte[]> requestPacketList = new ArrayList<>();
+    private ArrayList<byte[]> responsePacketList = new ArrayList<>();
+
+    public Client(SocketChannel socketChannel, Selector selector) throws IOException {
+        this.socketChannel = socketChannel;
+        socketChannel.configureBlocking(false);
+        this.selectionKey = socketChannel.register(selector, SelectionKey.OP_READ);
+        this.selectionKey.attach(this);
+    }
+
+    public SocketChannel getSocketChannel() { return this.socketChannel; }
+    public SelectionKey getSelectionKey() { return this.selectionKey; }
+    public Parser getRequestParser() { return this.requestParser; }
+    public String getUserNick() {
+        return this.userNick;
+    }
+    public ArrayList<byte[]> getRequestPacketList() {
+        return this.requestPacketList;
+    }
+    public ArrayList<byte[]> getResponsePacketList() { return this.responsePacketList; }
+
+    public void setUserNick(String userNick) { this.userNick = userNick; }
+    public void setResponsePacketList(ArrayList<byte[]> responsePacketList) { this.responsePacketList = responsePacketList; }
+
+    public void clearRequestPacketList() { this.requestPacketList.clear(); }
+    public void clearResponsePacketList() { this.responsePacketList.clear(); }
+
+    public Runnable makeWholePacket() {
+        Runnable runnable = () -> {
+          try {
+              ByteBuffer byteBuffer = ByteBuffer.allocate(120);
+              int byteCount = this.socketChannel.read(byteBuffer);
+
+              //상대방이 SocketChannel의 close() 메소드를 호출할 경우
+              if (byteCount == -1) {
+                  throw new IOException("클라이언트 연결 정상적으로 끊김" + socketChannel.getRemoteAddress());
+              }
+
+              // 정상 동작 시작
+              byteBuffer.flip();
+              byte[] requestPacket = byteBuffer.array();
+              this.requestPacketList.add(requestPacket);
+
+              if (!requestParser.isLast(requestPacket)) {
+                  this.selectionKey.interestOps(SelectionKey.OP_READ);
+              } else {
+                  Reader reader = new Reader(this);
+                  reader.deployWorker(this.requestParser.getFunctionName(this.requestPacketList));
+              }
+          } catch (IOException e) {
+              System.out.println("server receive IOException\n\n\n");
+              e.printStackTrace();
+          }
+        };
+        return runnable;
+    }
+}
