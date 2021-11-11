b27c1e789b837f512bda60d098d84ac7b4344db5
dongsub_youn <es001311@korea.ac.kr>
2021-11-11 16:28:31 +0900

    feat(server): channel에 write를 담당하는 Writer 클래스 추가

---
+++ server/src/main/java/server/Writer.java
@@ -0,0 +1,39 @@
+package server;
+
+import server.Worker.Worker;
+
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.channels.SelectionKey;
+
+public class Writer {
+    private Client client;
+
+    public Writer(Client client) {
+        this.client = client;
+    }
+
+    public Runnable writeToChannel() {
+        Runnable runnable = () -> {
+            try {
+                for (byte[] packet : this.client.getResponsePacketList()) {
+                    ByteBuffer byteBuffer = ByteBuffer.wrap(packet);
+                    this.client.getSocketChannel().write(byteBuffer);
+                }
+            } catch (IOException e) {
+                System.out.println("Writer IOException\t\t\t");
+                Server.getCallback().failed(e, null);
+                e.printStackTrace();
+                Worker.handleClientOut(this.client);
+            } catch (Exception e) {
+                System.out.println("Writer Exception\n\n\n");
+                Server.getCallback().failed(e, null);
+                e.printStackTrace();
+            }
+            this.client.clearResponsePacketList();
+            this.client.getSelectionKey().interestOps(SelectionKey.OP_READ);
+            Server.getCallback().completed(null, null);
+        };
+        return runnable;
+    }
+}
