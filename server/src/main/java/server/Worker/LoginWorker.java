8af5959e060341d4c34f663e7c2eaa252a749241
dongsub_youn <es001311@korea.ac.kr>
2021-11-11 16:43:15 +0900

    feat(server): LoginWorker 클래스 추가

---
+++ server/src/main/java/server/Worker/LoginWorker.java
@@ -0,0 +1,54 @@
+package server.Worker;
+
+import packet.ResponsePacket;
+import server.Client;
+import server.Server;
+
+import java.io.IOException;
+import java.nio.channels.SelectionKey;
+import java.nio.charset.StandardCharsets;
+
+public class LoginWorker implements Worker{
+    private Client client;
+
+    public LoginWorker(Client client) {
+        this.client = client;
+    }
+
+    @Override
+    public void doWork() {
+        try {
+            this.client.setUserNick(new String(this.client.getRequestParser().getContents(this.client.getRequestPacketList()), StandardCharsets.UTF_8));
+        } catch (IOException e) {
+            System.out.println("LoginWorker IOException\n\n\n");
+            e.printStackTrace();
+            Worker.handleClientOut(this.client);
+        }
+        this.client.clearRequestPacketList();
+
+        // Server에 출력
+        System.out.println(this.client.getUserNick() + "\t입장");
+
+        for (Client c : Server.getClientList()) {
+            if (!c.equals(this.client)) {
+                ResponsePacket responsePacket = new ResponsePacket(
+                        (byte) 20,
+                        (byte) 0,
+                        ("Server > " + this.client.getUserNick() + " 님이 입장하였습니다.").getBytes(StandardCharsets.UTF_8),
+                        "1".getBytes(StandardCharsets.UTF_8)
+                );
+                c.setResponsePacketList(responsePacket.responsePacketList);
+            } else {
+                ResponsePacket responsePacket = new ResponsePacket(
+                        (byte) 20,
+                        (byte) 0,
+                        ("Server > " + this.client.getUserNick() + " (으)로 로그인 완료").getBytes(StandardCharsets.UTF_8),
+                        "1".getBytes(StandardCharsets.UTF_8)
+                );
+                this.client.setResponsePacketList(responsePacket.responsePacketList);
+            }
+            c.getSelectionKey().interestOps(SelectionKey.OP_WRITE);
+        }
+        Server.getCallback().completed(null, null);
+    }
+}
