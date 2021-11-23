6107b4272e6db229b06f29df1203dc11d11ae2fa
dongsub_youn <es001311@korea.ac.kr>
2021-11-23 17:12:27 +0900

    fix(client): Queue를 이용한 channel write 기능 추가

--- server/src/main/java/server/worker/Worker.java
+++ server/src/main/java/server/worker/Worker.java
@@ -1,18 +1,42 @@
 package server.worker;
 
 import packet.ResponsePacket;
+import parser.Parser;
 import server.Client;
 import server.Server;
 import util.Constants;
-import util.Function;
 
 import java.io.IOException;
-import java.nio.channels.SelectionKey;
+import java.nio.ByteBuffer;
 import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
 import java.util.UUID;
 
 public interface Worker {
     void doWork();
+
+    static Runnable createWriteRunnable(Client client, ArrayList<byte[]> packetList) {
+        Runnable writeRunnable = () -> {
+            UUID uuid = Parser.getUUID(packetList.get(0));
+            try {
+                for (byte[] packet : packetList) {
+                    int byteCount = 0;
+                    ByteBuffer byteBuffer = ByteBuffer.wrap(packet);
+                    while (byteCount < Constants.PACKET_TOTAL_SIZE) {
+                        byteCount += client.getSocketChannel().write(byteBuffer);
+                    }
+                }
+            } catch (IOException e) {
+                System.out.println("Writer IOException\t\t\t");
+                e.printStackTrace();
+                Worker.handleClientOut(client, uuid);
+            } catch (Exception e) {
+                System.out.println("Writer Exception\n\n\n");
+                e.printStackTrace();
+            }
+        };
+        return writeRunnable;
+    }
 
     static void handleClientOut(Client client, UUID uuid) {
         try {
@@ -26,19 +50,13 @@ public interface Worker {
                             (client.getUserNick() + "님의 연결이 종료되었습니다.").getBytes(StandardCharsets.UTF_8),
                             "".getBytes(StandardCharsets.UTF_8)
                     );
-                    c.setResponsePacketList(uuid, responsePacket.responsePacketList);
-                    c.getSelectionKey().interestOps(SelectionKey.OP_WRITE);
-                } else {
-                    c.getSelectionKey().interestOps(SelectionKey.OP_READ);
+                    createWriteRunnable(c, responsePacket.responsePacketList);
                 }
-                c.clearRequestPacketList(uuid);
-                c.getSocketChannel().close();
+                client.getSocketChannel().close();
                 Server.setClientList(false, client);
-                Server.getCallback().completed(null, null);
             }
         } catch (IOException e) {
             System.out.println("socketChannel close IOException\n\n\n");
-            Server.getCallback().failed(e, null);
             e.printStackTrace();
         }
     }
