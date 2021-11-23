204c8acc9800a259b5cd80a4a5a0806bf9e5e98c
dongsub_youn <es001311@korea.ac.kr>
2021-11-23 17:13:52 +0900

    fix(client): Queue를 이용한 channel write로 responsePacketList 관련 로직 삭제

--- server/src/main/java/server/Client.java
+++ server/src/main/java/server/Client.java
@@ -1,10 +1,8 @@
 package server;
 
 import parser.*;
-import util.Constants;
 
 import java.io.IOException;
-import java.nio.ByteBuffer;
 import java.nio.channels.SelectionKey;
 import java.nio.channels.Selector;
 import java.nio.channels.SocketChannel;
@@ -14,13 +12,11 @@ import java.util.UUID;
 import java.util.concurrent.ConcurrentHashMap;
 
 public class Client {
-
     private SocketChannel socketChannel;
     private SelectionKey selectionKey;
     private Parser requestParser = new RequestParser();
     private String userNick;
     private Map<UUID, ArrayList<byte[]>> requestPacketListMap = new ConcurrentHashMap<>();
-    private Map<UUID, ArrayList<byte[]>> responsePacketListMap = new ConcurrentHashMap<>();
 
     public Client(SocketChannel socketChannel, Selector selector) throws IOException {
         this.socketChannel = socketChannel;
@@ -32,64 +28,13 @@ public class Client {
     public SocketChannel getSocketChannel() { return this.socketChannel; }
     public SelectionKey getSelectionKey() { return this.selectionKey; }
     public Parser getRequestParser() { return this.requestParser; }
-    public String getUserNick() {
-        return this.userNick;
-    }
-    public Map<UUID, ArrayList<byte[]>> getResponsePacketListMap() { return this.responsePacketListMap; }
+    public String getUserNick() { return this.userNick; }
     public Map<UUID, ArrayList<byte[]>> getRequestPacketListMap() { return this.requestPacketListMap; }
     public ArrayList<byte[]> getRequestPacketList(UUID uuid) {
         return this.requestPacketListMap.get(uuid);
     }
-    public ArrayList<byte[]> getResponsePacketList(UUID uuid) {
-        return this.responsePacketListMap.get(uuid);
-    }
 
     public void setUserNick(String userNick) { this.userNick = userNick; }
-    public void setResponsePacketList(UUID uuid, ArrayList<byte[]> responsePacketList) {
-        this.responsePacketListMap.put(uuid, responsePacketList);
-    }
 
     public void clearRequestPacketList(UUID uuid) { this.requestPacketListMap.remove(uuid); }
-    public void clearResponsePacketList(UUID uuid) { this.responsePacketListMap.remove(uuid); }
-
-//    public Runnable makeWholePacket() {
-//        Runnable runnable = () -> {
-//          try {
-//              ByteBuffer byteBuffer = ByteBuffer.allocate(Constants.PACKET_TOTAL_SIZE);
-//              int byteCount = this.socketChannel.read(byteBuffer);
-//
-//              //상대방이 SocketChannel의 close() 메소드를 호출할 경우
-//              if (byteCount == -1) {
-//                  System.out.println("클라이언트 연결 정상적으로 끊김" + socketChannel.getRemoteAddress());
-//                  Server.setClientList(false,this);
-//                  return;
-//              }
-//
-//              while ( 0 < byteCount && byteCount < Constants.PACKET_TOTAL_SIZE){
-//                  byteCount += this.socketChannel.read(byteBuffer);
-//              }
-//
-//              // 정상 동작 시작
-//              byteBuffer.flip();
-//              byte[] requestPacket = byteBuffer.array();
-//              UUID uuid = Parser.getUUID(requestPacket);
-//              if (!this.requestPacketListMap.containsKey(uuid)) {
-//                  this.requestPacketListMap.put(uuid, new ArrayList<>());
-//              }
-//              this.requestPacketListMap.get(uuid).add(requestPacket);
-//
-//              if (!Parser.isLast(requestPacket)) {
-//                  this.selectionKey.interestOps(SelectionKey.OP_READ);
-//                  Server.getCallback().completed(null, null);
-//              } else {
-//                  Reader reader = new Reader(this);
-//                  reader.deployWorker(uuid);
-//              }
-//          } catch (IOException e) {
-//              System.out.println("server receive IOException\n\n\n");
-//              e.printStackTrace();
-//          }
-//        };
-//        return runnable;
-//    }
 }
