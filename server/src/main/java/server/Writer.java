d372ef50dc10fded85a0dfe1dd249911796fb471
dongsub_youn <es001311@korea.ac.kr>
2021-11-23 17:11:27 +0900

    chore(client): 불필요한 코드 삭제

--- server/src/main/java/server/Writer.java
+++ server/src/main/java/server/Writer.java
@@ -1,15 +1,13 @@
 package server;
 
-import server.worker.Worker;
-import util.Constants;
+//import server.worker.Worker;
+//import util.Constants;
+//
+//import java.io.IOException;
+//import java.nio.ByteBuffer;
+//import java.util.UUID;
 
-import java.io.IOException;
-import java.nio.ByteBuffer;
-import java.nio.channels.SelectionKey;
-import java.util.ArrayList;
-import java.util.UUID;
 
-/*
 public class Writer {
     private Client client;
 
@@ -17,34 +15,29 @@ public class Writer {
         this.client = client;
     }
 
-    public Runnable writeToChannel() {
-        Runnable runnable = () -> {
-            for (UUID uuid : this.client.getResponsePacketListMap().keySet()) {
-                try {
-                    for (byte[] packet : this.client.getResponsePacketList(uuid)) {
-                        int byteCount = 0;
-                        ByteBuffer byteBuffer = ByteBuffer.wrap(packet);
-                        while (byteCount < Constants.PACKET_TOTAL_SIZE) {
-                            byteCount += this.client.getSocketChannel().write(byteBuffer);
-                        }
-                    }
-                } catch (IOException e) {
-                    System.out.println("Writer IOException\t\t\t");
-                    Server.getCallback().failed(e, null);
-                    e.printStackTrace();
-                    Worker.handleClientOut(this.client, uuid);
-                } catch (Exception e) {
-                    System.out.println("Writer Exception\n\n\n");
-                    Server.getCallback().failed(e, null);
-                    e.printStackTrace();
-                }
-                this.client.clearRequestPacketList(uuid);
-                this.client.clearResponsePacketList(uuid);
-            }
-            this.client.getSelectionKey().interestOps(SelectionKey.OP_READ);
-            Server.getCallback().completed(null, null);
-        };
-        return runnable;
-    }
+//    public Runnable writeToChannel() {
+//        Runnable runnable = () -> {
+//            for (UUID uuid : this.client.getResponsePacketListMap().keySet()) {
+//                try {
+//                    for (byte[] packet : this.client.getResponsePacketList(uuid)) {
+//                        int byteCount = 0;
+//                        ByteBuffer byteBuffer = ByteBuffer.wrap(packet);
+//                        while (byteCount < Constants.PACKET_TOTAL_SIZE) {
+//                            byteCount += this.client.getSocketChannel().write(byteBuffer);
+//                        }
+//                    }
+//                } catch (IOException e) {
+//                    System.out.println("Writer IOException\t\t\t");
+//                    e.printStackTrace();
+//                    Worker.handleClientOut(this.client, uuid);
+//                } catch (Exception e) {
+//                    System.out.println("Writer Exception\n\n\n");
+//                    e.printStackTrace();
+//                }
+//                this.client.clearRequestPacketList(uuid);
+//                this.client.clearResponsePacketList(uuid);
+//            }
+//        };
+//        return runnable;
+//    }
 }
-*/
