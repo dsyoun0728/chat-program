ad76420ff32d9ddbaf15a7497681ed2e66bd4e84
dongsub_youn <es001311@korea.ac.kr>
2021-11-23 11:00:54 +0900

    feat(client): 클라이언트에서 서버에 요청을 보내는 Writer 추가

---
+++ client/src/main/java/client/Writer.java
@@ -0,0 +1,45 @@
+package client;
+
+import parser.Parser;
+import util.Constants;
+
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.channels.SocketChannel;
+import java.util.ArrayList;
+
+public class Writer {
+    private Client client;
+
+    public Writer(Client client) {
+        this.client = client;
+    }
+
+    public void writeToChannel(ArrayList<byte[]> byteArrayList) {
+        Runnable writeRunnable = () -> {
+            try {
+                String fn = Parser.getFunctionName(byteArrayList.get(0));
+                if (fn.equals("SendFile")) {
+                    System.out.println("파일을 서버에 전송 중입니다....");
+                }
+                for (byte[] byteArray : byteArrayList) {
+                    int sendCount = 0;
+                    ByteBuffer byteBuffer = ByteBuffer.wrap(byteArray);
+                    while (sendCount < Constants.PACKET_TOTAL_SIZE) {
+                        sendCount += this.client.getSocketChannel().write(byteBuffer);
+                    }
+                }
+                if (fn.equals("SendFile")) {
+                    System.out.println("서버가 처리 중입니다....");
+                }
+            } catch (IOException e) {
+                System.out.println("client send IOException\n\n\n" + e + "\n\n\n");
+                this.client.stopClient();
+            } catch (Exception e) {
+                System.out.println("client send Exception\n\n\n" + e + "\n\n\n");
+                this.client.stopClient();
+            }
+        };
+        this.client.getExecutorService().submit(writeRunnable);
+    }
+}
